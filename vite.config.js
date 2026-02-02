fd9658604843a8eca46d42c729108f4989fecab5
Yourroot <licenseac@gmail.com>
2026-02-02 17:37:29 +0800

    解决支付bug

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: true, // 允许外部访问
+    allowedHosts: [
+      'dd.vpno.eu.org',
+      '.vpno.eu.org', // 允许所有 vpno.eu.org 的子域名
+      'localhost'
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
